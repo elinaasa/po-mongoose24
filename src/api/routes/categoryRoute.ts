f58cfb5bc935831420ca653738a0b02005d52c06
Elina Sankila <sankilaelina@gmail.com>
2024-08-23 11:45:07 +0300

    controllers, models, router added

--- src/api/routes/categoryRoute.ts
+++ src/api/routes/categoryRoute.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { postCategory } from '../controllers/categoryController';
-
+import { getCategory, putCategory, deleteCategory } from '../controllers/categoryController';
 const router = express.Router();
 
 router.route('/').post(postCategory);
+
+router.route('/:id').get(getCategory).put(putCategory).delete(deleteCategory);
+
 
 export default router;
