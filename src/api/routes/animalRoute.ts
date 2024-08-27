e5b238b31d17fae7c60bd2bc6d4cec1c76883111
Elina Sankila <sankilaelina@gmail.com>
2024-08-27 10:50:33 +0300

    getAnimalByBox

--- src/api/routes/animalRoute.ts
+++ src/api/routes/animalRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { deleteAnimal, getAnimal, getAnimals, postAnimal } from '../controllers/animalController';
-import { putAnimal } from '../controllers/animalController';
+import {putAnimal, deleteAnimal, getAnimal, getAnimals, postAnimal, getAnimalsByBox } from '../controllers/animalController';
 const router = express.Router();
 
 router.route('/').post(postAnimal).get(getAnimals);
 
+router.route('/location').get(getAnimalsByBox);
+
 router.route('/:id').get(getAnimal).put(putAnimal).delete(deleteAnimal);
+
 export default router;
